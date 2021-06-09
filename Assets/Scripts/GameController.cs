cf846e0435e459148c93e77d9dca61d9e6e2f595
havokentity <havokentity@gmail.com>
2021-06-10 00:28:45 +0530

    Added inventory item with toggle and fixed script execution order

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,13 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameController : MonoBehaviour
 {
+    public static GameController instance;
+    public List<ItemInventoryImageMapping> itemsMappingList;
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameController.instance = this;
+    }
+
+    public Sprite getSpriteFromItem(Item item)
+    {
+        Sprite returnSprite = null;
+        foreach(var itemMapping in itemsMappingList)
+        {
+            if(itemMapping.item == item)
+            {
+                returnSprite = itemMapping.image;
+                break;
+            }
+        }
+
+        return returnSprite;
     }
 
     // Update is called once per frame
--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -1,12 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+
+public enum Item
+{
+    NONE,
+    HAT_TYPE_1
+}
+
+[System.Serializable]
+public class ItemInventoryImageMapping
+{
+    public Item item;
+    public Sprite image;
+}
 
 public class InventoryItem : MonoBehaviour
 {
+    public Inventory parentInventory;
+
+    
+    public Item item;
+    public float shopPrice, sellingPrice;
+
+    public Image cellItemImage;
+
+    public void UpdateItem()
+    {
+        switch(item)
+        {
+            case Item.NONE:
+                cellItemImage.color = new Color(1, 1, 1, 0);
+                break;
+            default:
+                cellItemImage.sprite = GameController.instance.getSpriteFromItem(item);
+                cellItemImage.color = new Color(1, 1, 1, 1);
+                break;
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        UpdateItem();
     }
 }
