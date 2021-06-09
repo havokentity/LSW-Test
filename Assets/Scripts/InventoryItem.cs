f710fe1c874dd4732792bd8f6b65c58ec056655d
havokentity <havokentity@gmail.com>
2021-06-09 23:31:03 +0530

    Added first item to UI, added cells parent to Inventory

--- Assets/Scripts/InventoryItem.cs
+++ Assets/Scripts/InventoryItem.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Inventory : MonoBehaviour
+public class InventoryItem : MonoBehaviour
 {
-    public GameObject cellsParent;
-
     // Start is called before the first frame update
     void Start()
     {
